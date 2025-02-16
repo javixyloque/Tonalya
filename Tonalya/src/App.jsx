33dc22d64cdb14e18d0e54ac25f99216458263bd
javixyloque <perosa1000@gmail.com>
2025-02-16 14:50:17 +0100

    inicio del proyecto

---
+++ Tonalya/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [loading, setLoading] = useState(false)
+
+
+  if (!loading) {
+    
+  }
+
+  return (
+    <>
+      <h1>Bienvenido a Tonalya</h1>
+    </>
+  )
+}
+
+export default App
