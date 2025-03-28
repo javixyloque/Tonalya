747ee5e3d0d10fcc2075e30f890ff32ac8c3002e
javixyloque <perosa1000@gmail.com>
2025-03-28 01:02:58 +0100

    pruebas profesores

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode , BrowserRouter, Route, Switch } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,5 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+  
 )
