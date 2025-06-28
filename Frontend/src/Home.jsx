73f1d2ac29cb1b5fb42e1bb292987d08c25c0e7f
javixyloque <javixyloque@gmail.com>
2025-06-28 14:13:40 +0200

    Retoques Interfaz y Hoja de Estilos

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,11 +1,18 @@
-
+import { Link } from 'react-router-dom';
 // import './Home.css';
+
+// PALETA DE COLORES
+// https://colorhunt.co/palette/21344854779294b4c1ecefca
 
 const  Home = ()=> {
     return (
         <div className="home">
             <h1>Bienvenido a la página de inicio</h1>
-            <p>¡Bienvenido al sitio web de tonalya!</p>
+            <p>¡Bienvenido al sitio web de Tonalya!</p>
+            <Link to="/formclase" className="btn btn-primary">Formulario Clases</Link>
+            <br />
+            <Link to="/profesores" className="btn btn-secondary">Profesores</Link>
+            <br />
         </div>
     );
 }
