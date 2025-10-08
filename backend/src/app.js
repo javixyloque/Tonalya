d7222820a9e903b5620c7f36de092934d653af93
javixyloque <javixyloque@gmail.com>
2025-10-08 22:42:48 +0200

    peticiones http

--- backend/src/app.js
+++ backend/src/app.js
@@ -75,85 +75,199 @@ async function main() {
     })
 
 
-    
-    app.get ('/usuario/:nombre', async (req, res) => {
+    // PETICIONES
+
+    // CRUD USUARIO
+
+    // CREAR USUARIO
+    app.post('/usuario', async (req, res) => {
         try {
-            const nombre = req.params.nombre;
-            const usuario = await Usuario.findOne({ "nombre": nombre });
-            res.json (usuario);
-
-        } catch (ex) {
-            console.error("Error: ", ex);
-        }
-    })
-
-    // INICIO SESION PROFESOR
-    app.post ('/profesor/:email', async (req, res) => {
-        try {
-            const email = req.params.email;
-            const profesor = await Profesor.findOne({ "email" : email });
-            if (!profesor) {
-                return -1;
-            }
-
-            // COMPROBAR QUE CONTRASEÑA COINCIDE
-            const coincidePassword = await bcrypt.compare(req.params.password, profesor.password);
-
-            // FUNCION PARA COMPARAR CONTRASEÑA (NECESARIO POR HASHING)
-            if (coincidePassword) {
-               return profesor;
-            } else {
-                return 0;
-            }
-        } catch (ex) {
-            console.error("Error: ", ex);
-            return 0;
-        }
-    })
-
-    // PROFESOR => GET
-    // Obtener los profesores
-    app.get('/profesor', async (req, res) => {
-        try{
-            const profesores =  await Profesor.find({});
-            res.json(profesores);
-    } catch(error) {
-            res.status(500).json({ error: 'Ocurrió un error' });
-            
+            const usuario = new Usuario({
+                nombre: req.body.nombre,
+                email: req.body.email,
+                telefono: req.body.telefono,
+                password: req.body.password,
+                imagen: req.body.imagen,
+                tipo: 'ALUMNO'
+            });
+            await usuario.save();
+            res.json({ mensaje: 'Usuario creado exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al crear el usuario', error: error.message });
         }
     });
 
-    // PROFESOR => POST 
-        // Crear un nuevo profesor
-        // POST /profesor
-        app.post('/profesor', upload.single('imagen'),  async (req, res) => {
-        // Validar los datos
-        // if (!req.body.nombre || !req.body.email || !req.body.password || !req.body.telefono || req.body.provincia) {
-        //     return res.status(400).json({ mensaje: 'Todos los campos son obligatorios' });
-        // }
-    
+    // OBTENER DATOS USUARIO
+    app.get('/usuario/:id', async (req, res) => {
         try {
-        const email = req.params.email;
-        const profesor = await Profesor.findOne({ "email" : email });
-        if (!profesor) {
-            return -1;
+            const usuario = await Usuario.findById(req.params.id);
+            if (!usuario) {
+                return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+            }
+            res.json(usuario);
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al obtener el usuario', error: error.message });
         }
-        
-        // Comprobar que la contraseña coincide
-        const coincidePassword = await bcrypt.compare(req.body.password, profesor.password);
-        
-        // Función para comparar contraseña (necesario por hashing)
-        if (coincidePassword) {
-           return profesor;
-        } else {
-            return 0;
+    });
+
+    // MODIFICAR USUARIO
+    app.put('/usuario/:id', async (req, res) => {
+        try {
+            const usuario = await Usuario.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            if (!usuario) {
+                return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+            }
+            res.json(usuario);
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al actualizar el usuario', error: error.message });
         }
-    } catch (ex) {
-        console.error("Error: ", ex);
-        return 0;
-    }
-        
+    });
+
+    // BORRAR USUARIO
+    app.delete('/usuario/:id', async (req, res) => {
+        try {
+            await Usuario.findByIdAndDelete(req.params.id);
+            res.json({ mensaje: 'Usuario eliminado exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al eliminar el usuario', error: error.message });
+        }
+    });
+
+    // AÑADIR CLASE A UN ALUMNO
+    app.put('/alumno/:id/clase', async (req, res) => {
+        try {
+            const alumno = await Usuario.findById(req.params.id);
+            if (!alumno || alumno.tipo !== 'ALUMNO') {
+                return res.status(404).json({ mensaje: 'Alumno no encontrado o no es un alumno' });
+            }
+            
+            
+            const clase = await Clase.findById(req.body.claseId);
+            if (!clase) {
+                return res.status(404).json({ mensaje: 'Clase no encontrada' });
+            }
+            
+            
+            alumno.clases.push(clase._id);
+            await alumno.save();
+            
+            res.json({ mensaje: 'Clase agregada exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al agregar la clase', error: error.message });
+        }
+    });
+    
+    // INICIO SESIÓN ALUMNO
+
+    app.post('/alumno/login', async (req, res) => {
+        try {
+            const email = req.body.email;
+            const password = req.body.password;
+            
+            // Buscar el alumno con el correo electrónico y contraseña
+            const usuario = await Usuario.findOne({ "email": email });
+            if (!usuario || !bcrypt.compareSync(password, usuario.password)) {
+                return res.status(401).json({ mensaje: 'Correo electrónico o contraseña incorrectos' });
+            }
+            
+            // Almacenar el ID del usuario en sesión
+            sessionStorage.setItem('usuarioId', usuario._id);
+            
+            res.json({ mensaje: 'Iniciaste sesión exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al iniciar sesión como alumno', error: error.message });
+        }
+    });
+    
+    // CRUD PROFESOR
+
+    // INICIO SESION PROFESOR
+    app.post('/profesor/login', async (req, res) => {
+        try {
+            const email = req.body.email;
+            const password = req.body.password;
+            
+            // Buscar el profesor con el correo electrónico y contraseña
+            const profesor = await Profesor.findOne({ "email": email });
+            if (!profesor || !bcrypt.compareSync(password, profesor.password)) {
+                return res.status(401).json({ mensaje: 'Correo electrónico o contraseña incorrectos' });
+            }
+            
+            // Almacenar el ID del usuario en sesión
+            res.json({ mensaje: 'Iniciaste sesión exitosamente', usuario: profesor.nombre, id: profesor.id });
+            sessionStorage.setItem('usuarioId', profesor._id);
+            
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al iniciar sesión como profesor', error: error.message });
+        }
+    });
+    // AÑADIR PROFESOR
+    app.post('/profesor', async (req, res) => {
+        try {
+            const profesor = new Profesor({
+                nombre: req.body.nombre,
+                email: req.body.email,
+                telefono: req.body.telefono,
+                password: req.body.password,
+                imagen: req.body.imagen,
+                provincia: req.body.provincia
+            });
+            await profesor.save();
+            res.json({ mensaje: 'Profesor creado exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al crear el profesor', error: error.message });
+        }
+    });
+
+    // OBTENER DATOS PROFESOR
+    app.get('/profesor/:id', async (req, res) => {
+        try {
+            const profesor = await Profesor.findById(req.params.id);
+            if (!profesor) {
+                return res.status(404).json({ mensaje: 'Profesor no encontrado' });
+            }
+            res.json(profesor);
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al obtener el profesor', error: error.message });
+        }
+    });
+
+    app.get(('/profesor'), async (req, res) => {
+        try {
+            const profesores = await Profesor.find({});
+            if (!profesores) {
+                res.json({mensaje: "No se ha encontrado ningun profesor"})
+            } else {
+                res.json(profesores);
+            }
+        } catch (error) {
+            res.status(500).json({mensaje: "Error al obtener los profesores", error: error.message})
+        }
     })
+
+    // ACTUALIZAR DATOS PROFESOR
+    app.put('/profesor/:id', async (req, res) => {
+        try {
+            const profesor = await Profesor.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            if (!profesor) {
+                return res.status(404).json({ mensaje: 'Profesor no encontrado' });
+            }
+            res.json(profesor);
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al actualizar el profesor', error: error.message });
+        }
+    });
+
+    // BORRAR DATOS PROFESOR
+    app.delete('/profesor/:id', async (req, res) => {
+        try {
+            await Profesor.findByIdAndDelete(req.params.id);
+            res.json({ mensaje: 'Profesor eliminado exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al eliminar el profesor', error: error.message });
+        }
+    });
+
     // ANADIR INSTRUMENTO/S PROFESOR
     app.put('/profesor/:id/instrumento', async (req, res) => {
         try {
@@ -174,19 +288,91 @@ async function main() {
             res.status(500).json({ mensaje: 'Error al agregar el instrumento' });
         }
     });
-    
-    // BORRAR CUENTA
-    app.delete('/profesor/:id', async (req, res) => {
-        try {
-            const profesor = await Profesor.findByIdAndDelete(req.params.id);
+
+
+    // AÑADIR CLASE PROFESOR
+    app.put('/profesor/:id/clase', async (req, res) => {
+    try {
+            const profesor = await Profesor.findById(req.params.id);
             if (!profesor) {
                 return res.status(404).json({ mensaje: 'Profesor no encontrado' });
             }
-            res.json({ mensaje: 'Profesor eliminado exitosamente' });
+            
+            
+            const clase = await Clase.findById(req.body.claseId);
+            if (!clase) {
+                return res.status(404).json({ mensaje: 'Clase no encontrada' });
+            }
+            
+            
+            profesor.clases.push(clase._id);
+            await profesor.save();
+            
+            res.json({ mensaje: 'Clase agregada exitosamente' });
         } catch (error) {
-            res.status(500).json({ mensaje: 'Error al eliminar el profesor' });
+            res.status(500).json({ mensaje: 'Error al agregar la clase', error: error.message });
         }
     });
+
+
+    // CRUD CLASE
+
+    // AÑADIR CLASE
+    app.post('/clase', async (req, res) => {
+        try {
+            const clase = new Clase({
+                titulo: req.body.titulo,
+                descripcion: req.body.descripcion,
+                precio: req.body.precio,
+                imagen: req.body.imagen,
+                fecha: req.body.fecha,
+                asistencia: req.body.asistencia,
+                pagoRealizado: req.body.pagoRealizado
+            });
+            await clase.save();
+            res.json({ mensaje: 'Clase creada exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al crear la clase', error: error.message });
+        }
+    });
+
+    // OBTENER DATOS CLASE
+    app.get('/clase/:id', async (req, res) => {
+        try {
+            const clase = await Clase.findById(req.params.id);
+            if (!clase) {
+                return res.status(404).json({ mensaje: 'Clase no encontrada' });
+            }
+            res.json(clase);
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al obtener la clase', error: error.message });
+        }
+    });
+
+    // MODIFICAR CLASE
+    app.put('/clase/:id', async (req, res) => {
+        try {
+            const clase = await Clase.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            if (!clase) {
+                return res.status(404).json({ mensaje: 'Clase no encontrada' });
+            }
+            res.json(clase);
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al actualizar la clase', error: error.message });
+        }
+    });
+
+    // BORRAR CLASE
+    app.delete('/clase/:id', async (req, res) => {
+        try {
+            await Clase.findByIdAndDelete(req.params.id);
+            res.json({ mensaje: 'Clase eliminada exitosamente' });
+        } catch (error) {
+            res.status(500).json({ mensaje: 'Error al eliminar la clase', error: error.message });
+        }
+    });
+    
+    
     
     // CREAR LOS INSTRUMENTOS A PARTIR DEL ARRAY DE INSTRUMENTOS
     app.get('/instrumentos', async (req,res) => {
